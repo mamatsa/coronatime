26eaca9cf72dd703c46802babe151182aafbf3bc
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-06-02 16:20:01 +0400

    chore: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grayish: '#808189',
+        'error-red': '#CC1E1E',
+        'main-green': '#0FBA68',
+        'main-blue': '#2029F3',
+      },
+      backgroundImage: {
+        'input-success': "url('assets/images/Success.svg')",
+      },
+    },
+    fontFamily: {
+      Inter: ['Inter', 'sans-serif'],
+    },
+  },
+  backgroundPosition: {
+    'right-4': 'right 2rem',
   },
   plugins: [],
 };

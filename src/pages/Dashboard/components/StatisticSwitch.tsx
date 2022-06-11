c9b08ef015c0bec0c8f69209a4e81dea8c30613b
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-06-12 00:09:16 +0400

    refactor: update covid statistics switch logic

--- src/pages/Dashboard/components/StatisticSwitch.tsx
+++ src/pages/Dashboard/components/StatisticSwitch.tsx
@@ -5,14 +5,14 @@ import { NavLink } from 'react-router-dom';
 const StatisticSwitch = () => {
   const { t } = useTranslation();
 
-  const activeClass = 'font-bold border-b-[3px] border-black pb-4';
-
   return (
     <ul className='flex gap-8 mt-10'>
       <li>
         <NavLink
           to='/'
-          className={({ isActive }) => (isActive ? activeClass : undefined)}
+          className={({ isActive }) =>
+            isActive ? 'font-bold border-b-[3px] border-black pb-4' : undefined
+          }
           id='dashboardWorldwideNavLink'
         >
           {t('dashboard.worldwide')}
@@ -21,7 +21,9 @@ const StatisticSwitch = () => {
       <li>
         <NavLink
           to='/by-country'
-          className={({ isActive }) => (isActive ? activeClass : undefined)}
+          className={({ isActive }) =>
+            isActive ? 'font-bold border-b-[3px] border-black pb-4' : undefined
+          }
           id='dashboardCountriesNavLink'
         >
           {t('dashboard.by_country')}

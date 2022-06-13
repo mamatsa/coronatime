53aef4f14b431f06c1f22e82fba258a0ec7e8a33
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-06-13 16:38:16 +0400

    refactor: remove any type from input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { ErrorMark } from 'components/svg';
 import { useTranslation } from 'react-i18next';
+import { UseFormRegister } from 'react-hook-form';
+import { FieldErrors } from 'react-hook-form';
+
+let hasTip: boolean = false;
 
 const Input: React.FC<{
   id: string;
@@ -9,16 +13,42 @@ const Input: React.FC<{
   placeholder: string;
   name: string;
   required: boolean;
-  register?: any;
+  register?: UseFormRegister<any>;
   minLength?: number;
   customValidation?: { func: () => boolean; message: string };
-  errors?: any;
+  errors?: FieldErrors;
   isDirty?: boolean | undefined;
   tip?: string;
 }> = (props) => {
-  const hasTip: boolean = !props.errors[props.name] && !props.isDirty;
+  if (props.errors) {
+    hasTip = !props.errors[props.name] && !props.isDirty;
+  }
 
   const { t } = useTranslation();
+
+  let register = {};
+  if (props.register) {
+    register = {
+      ...props.register(props.name, {
+        required: {
+          value: props.required,
+          message: 'errors.field_required',
+        },
+
+        ...(props.name === 'email' && {
+          pattern: {
+            value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            message: 'errors.valid_email',
+          },
+        }),
+        minLength: {
+          value: props.minLength ?? 0,
+          message: 'errors.min_symbols',
+        },
+        validate: props.customValidation?.func,
+      }),
+    };
+  }
 
   return (
     <div className={'flex flex-col'}>
@@ -29,29 +59,17 @@ const Input: React.FC<{
         id={props.id}
         type={props.type}
         placeholder={props.placeholder}
-        {...props.register(props.name, {
-          required: {
-            value: props.required,
-            message: 'errors.field_required',
-          },
-
-          ...(props.name === 'email' && {
-            pattern: {
-              value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-              message: 'errors.valid_email',
-            },
-          }),
-          minLength: {
-            value: props.minLength,
-            message: 'errors.min_symbols',
-          },
-          validate: props.customValidation?.func,
-        })}
+        {...register}
         className={`px-5 py-3 mt-2 text-black border-dark-brown border-[1px] rounded-lg w-full outline-main-blue ${
+          props.errors &&
           props.isDirty &&
           !props.errors[props.name] &&
           ' border-main-green outline-main-green  bg-input-success bg-no-repeat bg-[center_right_2rem]'
-        } ${props.errors[props.name] && 'border-error-red outline-error-red'}`}
+        } ${
+          props.errors &&
+          props.errors[props.name] &&
+          'border-error-red outline-error-red'
+        }`}
       />
 
       <p
@@ -61,10 +79,13 @@ const Input: React.FC<{
         id={`${props.id}InputError`}
       >
         {hasTip && props.tip}
-        {(props.errors[props.name]?.message ||
-          props.errors[props.name]?.type === 'validate') && <ErrorMark />}
-        {t(props.errors[props.name]?.message, { symbols: props.minLength })}
-        {props.errors[props.name]?.type === 'validate' &&
+        {props.errors &&
+          (props.errors[props.name]?.message ||
+            props.errors[props.name]?.type === 'validate') && <ErrorMark />}
+        {props.errors &&
+          t(props.errors[props.name]?.message, { symbols: props.minLength })}
+        {props.errors &&
+          props.errors[props.name]?.type === 'validate' &&
           props.customValidation &&
           t(props.customValidation.message)}
       </p>
